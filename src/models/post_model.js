5bc885a54a7729d85cfdf10962c110ae44afae13
zjohnson5455 <zacharyjohnsonri@gmail.com>
2018-05-03 14:04:58 -0400

    wired things up. need to test with first pass test

---
+++ src/models/post_model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from 'mongoose';
+
+// create a PostSchema with a title field
+
+const PostSchema = new Schema({
+  title: String,
+  // cover_url: String,
+  // tags: String,
+  // content: String,
+}, {
+  toJSON: {
+    virtuals: false,
+  },
+});
+
+// create PostModel class from schema
+const PostModel = mongoose.model('Post', PostSchema);
+
+
+export default PostModel;
