dd358ca5bd367d3dad287ee23291fb78295cd671
zjohnson5455 <zacharyjohnsonri@gmail.com>
2018-05-14 16:12:14 -0400

    changed router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import * as Posts from './controllers/post_controller';
-// import * as UserController from './controllers/user_controller';
-// import { requireAuth, requireSignin } from './services/passport';
+import * as UserController from './controllers/user_controller';
+import { requireAuth, requireSignin } from './services/passport';
 
 
 const router = Router();
@@ -12,19 +12,16 @@ router.get('/', (req, res) => {
 
 // /your routes will go here
 
-/* router.post('/signin', requireSignin, UserController.signin);
+router.post('/signin', requireSignin, UserController.signin);
 router.post('/signup', UserController.signup);
-router.post(requireAuth, Posts.createPost);
-router.post(requireAuth, Posts.updatePost);
-router.post(requireAuth, Posts.deletePost); */
 
 router.route('/posts')
-  .post(Posts.createPost)
+  .post(requireAuth, Posts.createPost)
   .get(Posts.getPosts);
 
 router.route('/posts/:id')
   .get(Posts.getPost)
-  .put(Posts.updatePost)
-  .delete(Posts.deletePost);
+  .put(requireAuth, Posts.updatePost)
+  .delete(requireAuth, Posts.deletePost);
 
 export default router;
